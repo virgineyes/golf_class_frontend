3504abb8ea39c5c9c83e5993cceb6b5379c9ad9d
ace.ca.chiu <ace.ca.chiu@deltaww.com>
2021-02-26 15:29:39 +0800

    'change-azure-serer-2'

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    chainWebpack: config => {
-      config
-        .plugin('html')
-        .tap(args => {
-          args[0].title= '台灣高爾夫學院報名表'
-          return args
-        })
-    }
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title= '台灣高爾夫學院報名表'
+        return args
+      })
   }
+}
